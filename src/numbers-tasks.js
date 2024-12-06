56961be777d729e3f6fa05e27b5f664d2f7f81a3
Abdyl-Aziz <azizshihabov@mail.ru>
2024-12-06 22:19:43 +0300

    feat: add solution for getSumOfDigits task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -298,8 +298,11 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return String(num)
+    .split('')
+    .map((item) => parseInt(item, 10))
+    .reduce((acc, i) => acc + i, 0);
 }
 
 /**
